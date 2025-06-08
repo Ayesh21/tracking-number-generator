2bbb16c79e9c198c35881a55eb48880bed1d5d13
Ayesh Navindu <navindu35@gmail.com>
2025-06-08 23:44:20 +0530

    adding validation

--- src/main/java/com/tracking/number/generator/dto/TrackingNumberRequest.java
+++ src/main/java/com/tracking/number/generator/dto/TrackingNumberRequest.java
@@ -1,5 +1,9 @@
 package com.tracking.number.generator.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 import java.time.Instant;
 import java.util.UUID;
 
@@ -7,11 +11,11 @@ import java.util.UUID;
  * The type Tracking number request.
  */
 public record TrackingNumberRequest(
-        String originCountryId,
-        String destinationCountryId,
-        double weight,
-        Instant createdAt,
-        UUID customerId,
-        String customerName,
-        String customerSlug
+        @NotBlank(message = "Origin country ID must not be blank") String originCountryId,
+        @NotBlank(message = "Destination country ID must not be blank") String destinationCountryId,
+        @Positive(message = "Weight must be positive") double weight,
+        @NotNull(message = "Created date/time must not be null") Instant createdAt,
+        @NotNull(message = "Customer ID must not be null") UUID customerId,
+        @NotBlank(message = "Customer name must not be blank") String customerName,
+        @NotBlank(message = "Customer slug must not be blank") String customerSlug
 ) {}
